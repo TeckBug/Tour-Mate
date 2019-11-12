2d24c869a3d9de8ac80eb945932b407957f89818
Shonpnil <shopnershopnil44@gmail.com>
2019-11-12 21:59:30 +0600

    main menu added

--- app/src/main/java/com/example/tourmate/MainActivity.java
+++ app/src/main/java/com/example/tourmate/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tourmate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.settingMenuItemId:
+                Toast.makeText(this, ""+item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.aboutMenuItemId:
+                Toast.makeText(this, ""+item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
