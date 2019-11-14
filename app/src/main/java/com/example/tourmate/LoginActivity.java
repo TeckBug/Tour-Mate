aa54b92bf9673a80823bde7509acbc7d6f08dc35
urahamat01 <urahamat01@gmail.com>
2019-11-14 15:38:03 +0600

    login

--- app/src/main/java/com/example/tourmate/LoginActivity.java
+++ app/src/main/java/com/example/tourmate/LoginActivity.java
@@ -3,12 +3,27 @@ package com.example.tourmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText emailET, passwordET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        init();
+    }
+
+    private void init() {
+        emailET = findViewById(R.id.emailETId);
+        passwordET= findViewById(R.id.passwordeETId);
+    }
+
+    public void singup(View view) {
+
     }
 }
