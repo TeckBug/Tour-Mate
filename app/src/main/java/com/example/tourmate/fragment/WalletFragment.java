932be9d61ff5b43373d7232ca8c5d792f088718a
Shonpnil <shopnershopnil44@gmail.com>
2019-11-22 23:42:26 +0600

    current weather create

--- app/src/main/java/com/example/tourmate/fragment/WalletFragment.java
+++ app/src/main/java/com/example/tourmate/fragment/WalletFragment.java
@@ -1,6 +1,7 @@
 package com.example.tourmate.fragment;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,10 +9,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.androdocs.httprequest.HttpRequest;
 import com.example.tourmate.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class WalletFragment extends Fragment {
+
+    String CITY = "sylhet,bd";
+    //String API = "8118ed6ee68db2debfaaa5a44c832918";
+    String API = "56768256d8dd4ab41df838bf77f5fa5b";
+
+    private ProgressBar loader;
+    private RelativeLayout mainContainer;
+
+    private TextView addressTxt, updated_atTxt, statusTxt, tempTxt, temp_minTxt, temp_maxTxt,
+            sunriseTxt, sunsetTxt, windTxt, pressureTxt, humidityTxt, errorTV;
 
     public WalletFragment() {
         // Required empty public constructor
@@ -20,7 +42,99 @@ public class WalletFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_wallet, container, false);
+        View view = inflater.inflate(R.layout.fragment_wallet, container, false);
+
+        init(view);
+
+        new weatherTask().execute();
+
+        return view;
+    }
+
+    class weatherTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            /* Showing the ProgressBar, Making the main design GONE */
+            loader.setVisibility(View.VISIBLE);
+            mainContainer.setVisibility(View.GONE);
+            errorTV.setVisibility(View.GONE);
+        }
+
+        protected String doInBackground(String... args) {
+            String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONObject sys = jsonObj.getJSONObject("sys");
+                JSONObject wind = jsonObj.getJSONObject("wind");
+                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+                Long updatedAt = jsonObj.getLong("dt");
+                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                String temp = main.getString("temp") + "°C";
+                String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
+                String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
+                String pressure = main.getString("pressure");
+                String humidity = main.getString("humidity");
+
+                Long sunrise = sys.getLong("sunrise");
+                Long sunset = sys.getLong("sunset");
+                String windSpeed = wind.getString("speed");
+                String weatherDescription = weather.getString("description");
+
+                String address = jsonObj.getString("name") + ", " + sys.getString("country");
+
+
+                /* Populating extracted data into our views */
+                addressTxt.setText(address);
+                updated_atTxt.setText(updatedAtText);
+                statusTxt.setText(weatherDescription.toUpperCase());
+                tempTxt.setText(temp);
+                temp_minTxt.setText(tempMin);
+                temp_maxTxt.setText(tempMax);
+                sunriseTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
+                sunsetTxt.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
+                windTxt.setText(windSpeed+" km/h");
+                pressureTxt.setText(pressure);
+                humidityTxt.setText(humidity);
+
+                /* Views populated, Hiding the loader, Showing the main design */
+                loader.setVisibility(View.GONE);
+                mainContainer.setVisibility(View.VISIBLE);
+
+
+            } catch (JSONException e) {
+                loader.setVisibility(View.GONE);
+                errorTV.setVisibility(View.VISIBLE);
+            }
+
+        }
+    }
+
+    private void init(View view) {
+        mainContainer = view.findViewById(R.id.mainContainerId);
+        addressTxt = view.findViewById(R.id.address);
+        updated_atTxt = view.findViewById(R.id.updated_at);
+        statusTxt = view.findViewById(R.id.status);
+        tempTxt = view.findViewById(R.id.temp);
+        temp_minTxt = view.findViewById(R.id.temp_min);
+        temp_maxTxt = view.findViewById(R.id.temp_max);
+        sunriseTxt = view.findViewById(R.id.sunrise);
+        sunsetTxt = view.findViewById(R.id.sunset);
+        windTxt = view.findViewById(R.id.wind);
+        pressureTxt = view.findViewById(R.id.pressure);
+        humidityTxt = view.findViewById(R.id.humidity);
+        loader = view.findViewById(R.id.loaderId);
+        errorTV = view.findViewById(R.id.errorTextId);
     }
 
 }
